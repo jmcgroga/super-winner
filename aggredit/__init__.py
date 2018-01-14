e41256aab4bf1c56f6a8bddb45b28016625189a3
James McGrogan <jmcgroga@gmail.com>
2018-01-13 23:04:05 -0500

    Updated aggredit.__init__.py to set url_prefix for Blueprints from configuration.  Moved config.py to app module and refactored to use config objects.  Updated run.py to use Development config by default.

--- aggredit/__init__.py
+++ aggredit/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 
 from .root import root
@@ -6,7 +8,13 @@ from .api import api
 # {{ module_import }}
 
 app = Flask(__name__)
-app.register_blueprint(root, url_prefix='/')
-app.register_blueprint(editor, url_prefix='/editor')
-app.register_blueprint(api, url_prefix='/api')
+flask_config = 'aggredit.config.Production'
+
+if 'FLASK_CONFIG' in os.environ:
+    flask_config = os.environ['FLASK_CONFIG']
+
+app.config.from_object(flask_config)
+app.register_blueprint(root, url_prefix=app.config['BLUEPRINT_ROOT_URL_PREFIX'])
+app.register_blueprint(editor, url_prefix=app.config['BLUEPRINT_ROOT_EDITOR_PREFIX'])
+app.register_blueprint(api, url_prefix=app.config['BLUEPRINT_ROOT_API_PREFIX'])
 # {{ module_blueprint }}
--- run.py
+++ run.py
@@ -1,3 +1,7 @@
+import os
 from aggredit import app
+
+if 'FLASK_CONFIG' not in os.environ:
+    os.environ['FLASK_CONFIG'] = 'aggredit.config.Development'
 
 app.run(host='0.0.0.0', port=5000, debug=True)
